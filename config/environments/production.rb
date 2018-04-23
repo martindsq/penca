bd6d1d660b0a0c6f079564d9636f344a513ae57e
Martin Dutra <martindsq@hotmail.com>
2018-04-23 19:30:29 -0300

    Update host for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'https://penca.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'penca.herokuapp.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
