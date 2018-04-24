e0ecaf5a4461ab9a684e75e76f2c3df1397c0d9c
Martin Dutra <martindsq@hotmail.com>
2018-04-24 20:04:56 -0300

    Configure laresiliafs domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,13 +27,13 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'penca.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'penca.laresiliafs.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILGUN_SMTP_SERVER'],
     port:                 ENV['MAILGUN_SMTP_PORT'],
-    domain:               'penca.herokuapp.com',
+    domain:               'penca.laresiliafs.com',
     user_name:            ENV['MAILGUN_SMTP_LOGIN'],
     password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
