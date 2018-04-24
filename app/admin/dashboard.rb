8f17c46e45510fab2d73d96080d20f0d29fd4f1b
Martin Dutra <martindsq@hotmail.com>
2018-04-24 17:39:26 -0300

    Allow admin to create users manuallyw

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -6,13 +6,23 @@ ActiveAdmin.register_page "Dashboard" do
     columns do
       column do
         panel I18n.t("active_admin.ranking") do
-          table_for User.invitation_accepted.sort{|a,b| a.points <=> b.points} do
+          table_for User.invitation_accepted.sort{|a,b| a.points <=> b.points}.first(20) do
             column :alias
             column :email
             column :points
           end
         end
       end
+      column do
+        panel I18n.t("active_admin.pending_users").first(20) do
+          table_for User.invitation_not_accepted do
+            column :email
+            column :invitation_sent_at
+            column :invited_by
+          end
+        end
+      end
+
     end 
   end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
   
-  permit_params :email
+  permit_params :email, :alias, :password, :password_confirmation
   
   filter :alias
   filter :email
@@ -15,6 +15,7 @@ ActiveAdmin.register User do
     column :email
     column :invitation_sent_at
     column :invitation_accepted_at
+    column :invited_by
     actions
   end
 
@@ -22,10 +23,14 @@ ActiveAdmin.register User do
 	@user = User.new
   end 
 
+  collection_action :new_user do
+    @user = User.new
+  end
+
   collection_action :send_invitation, :method => :post do
-	@user = User.invite!(permitted_params[:user], current_user)
+	@user = User.invite!(permitted_params[:user], current_admin)
 	if @user.errors.empty?
-		flash[:notice] = "User has been successfully invited."
+      flash[:notice] = "User has been successfully invited. " + accept_user_invitation_url({invitation_token: @user.raw_invitation_token})
 		redirect_to admin_users_path
 	else
 		messages = @user.errors.full_messages.map { |msg| msg }.join
@@ -34,8 +39,29 @@ ActiveAdmin.register User do
 	end
   end
 
+  collection_action :create_user, :method => :post do
+    now = Time.now
+    devise_invitable_params = {:invitation_created_at => now,
+                               :invitation_sent_at => now,
+                               :invitation_accepted_at => now,
+                               :invited_by => current_admin}
+    @user = User.create(permitted_params[:user].merge(devise_invitable_params))
+    if @user.errors.empty?
+      flash[:notice] = "User has been successfully created."
+      redirect_to admin_users_path
+    else
+      messages = @user.errors.full_messages.map { |msg| msg }.join
+      flash[:error] = "Error: " + messages
+      redirect_to new_user_admin_users_path
+    end
+  end
+
   action_item do
-    link_to 'Invite New User', new_invitation_admin_users_path
+    link_to t('invite_new_user'), new_invitation_admin_users_path
+  end
+
+  action_item do
+    link_to t('create_new_user'), new_user_admin_users_path
   end
 
   
