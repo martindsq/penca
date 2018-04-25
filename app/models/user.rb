20d85db8230a40bbe84751a4b286d048fa25653f
Martin Dutra <martindsq@hotmail.com>
2018-04-25 15:10:06 -0300

    Validate length of alias

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :alias, :on => :update
   validates_uniqueness_of :alias, :on => :update
+  validates :alias, length: { in: 3..20 }, on: :update
   devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          :authentication_keys => [:alias]
